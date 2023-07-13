754899a8ace6cea3d46d5fd254e6f87967f04061
Bruce <bruce@redravencomputing.com>
2023-07-13 01:31:17 -0500

    Added new test and late init for tasks

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -4,16 +4,11 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.core.IsNot.not
-import org.hamcrest.core.IsNull.notNullValue
-import org.hamcrest.core.IsNull.nullValue
-
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 
@@ -21,20 +16,37 @@ import org.robolectric.annotation.Config
 @Config(manifest = Config.NONE)
 class TasksViewModelTest {
 
+	// Subject under test
+	private lateinit var tasksViewModel: TasksViewModel
+
 	@get:Rule
 	var instantExecutorRule = InstantTaskExecutorRule()
 
+	// Subject under test private lateinit var tasksViewModel: TasksViewModel
+	@Before
+	fun setupViewModel() {
+		tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+	}
+
 	@Test
 	fun addNewTask_setsNewTaskEvent() {
-		//Given a fresh ViewModel
-		val taskViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
 
 		//When adding a new task
-		taskViewModel.addNewTask()
+		tasksViewModel.addNewTask()
 
 		// Then the new task event is triggered
-		val value = taskViewModel.newTaskEvent.getOrAwaitValue()
+		val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
 
-		assertThat(value.getContentIfNotHandled(), `is`(notNullValue()))
+		assertNotNull(value.getContentIfNotHandled())
+	}
+
+	@Test
+	fun setFilterAllTasks_taskAddViewVisible() {
+
+		// When the filter type is ALL_TASKS
+		tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+
+		// Then the "Add task" action is visible
+		assertTrue(tasksViewModel.tasksAddViewVisible.getOrAwaitValue())
 	}
 }
